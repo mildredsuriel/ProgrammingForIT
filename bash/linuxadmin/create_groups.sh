b371e0fec9f0729d97762cdea301618d39acbb8c
Mildred Suriel <mildred_suriel@live.com>
2020-11-11 15:22:08 -0500

    Update create_groups.sh

--- bash/linuxadmin/create_groups.sh
+++ bash/linuxadmin/create_groups.sh
@@ -1,39 +1,39 @@
 # Create top level policies
-sudo mkdir /home/shared/policies
+sudo mkdir /home/common/policies
 # Make file read only
-chmod a=rx /home/shared/policies
+chmod a=rx /home/common/policies
 
 sudo groupadd temp
 sudo groupadd staff
 sudo groupadd developers
 sudo groupadd admin
 
-sudo mkdir -p /home/shared/groups/temp
-sudo mkdir    /home/shared/groups/staff
-sudo mkdir    /home/shared/groups/developers
-sudo mkdir    /home/shared/groups/admin
+sudo mkdir -p /home/common/groups/temp
+sudo mkdir    /home/common/groups/staff
+sudo mkdir    /home/common/groups/developers
+sudo mkdir    /home/common/groups/admin
 
-sudo chgrp -R temp /home/shared/groups/temp
-sudo chgrp -R staff /home/shared/groups/staff
-sudo chgrp -R developers /home/shared/groups/developers
-sudo chgrp -R admin /home/shared/groups/admin
+sudo chgrp -R temp /home/common/groups/temp
+sudo chgrp -R staff /home/common/groups/staff
+sudo chgrp -R developers /home/common/groups/developers
+sudo chgrp -R admin /home/common/groups/admin
 
-sudo chmod -R 2775 /home/shared/groups/temp
-sudo chmod -R 2775 /home/shared/groups/staff
-sudo chmod -R 2775 /home/shared/groups/developers
-sudo chmod -R 2775 /home/shared/groups/admin
+sudo chmod -R 2775 /home/common/groups/temp
+sudo chmod -R 2775 /home/common/groups/staff
+sudo chmod -R 2775 /home/common/groups/developers
+sudo chmod -R 2775 /home/common/groups/admin
 
 # Make policy folder for each group
-sudo mkdir -p /home/shared/groups/temp/policies
-sudo mkdir    /home/shared/groups/staff/policies
-sudo mkdir    /home/shared/groups/developers/policies
-sudo mkdir    /home/shared/groups/admin/policies
+sudo mkdir -p /home/common/groups/temp/policies
+sudo mkdir    /home/common/groups/staff/policies
+sudo mkdir    /home/common/groups/developers/policies
+sudo mkdir    /home/common/groups/admin/policies
 
 # Make each policy folder read only
-sudo chmod a=rx /home/shared/groups/temp/policies
-sudo chmod a=rx /home/shared/groups/staff/policies
-sudo chmod a=rx /home/shared/groups/developers/policies
-sudo chmod a=rx /home/shared/groups/admin/policies
+sudo chmod a=rx /home/common/groups/temp/policies
+sudo chmod a=rx /home/common/groups/staff/policies
+sudo chmod a=rx /home/common/groups/developers/policies
+sudo chmod a=rx /home/common/groups/admin/policies
 
 # Get the OS name to determine what program to install
 os=$(hostnamectl | awk -F ' ' '/Operating System:/ {print $3}')
@@ -51,8 +51,10 @@ then
 	sudo yum install quota
 fi
 
-# Update fstab to support user and group quotas
-sudo sed -i '1 s/defaults/usrquota,grpquota/' /etc/fstab
+# Find the line within /etc/fstab that contains the "/" mount, and print the line number
+line_num=$(awk -F ' ' '$2 == "\/" {print NR}' /etc/fstab)
+# Update "/" fstab mount to support user and group quotas
+sudo sed -i "$line_num s/defaults/usrquota,grpquota/" /etc/fstab
 # Remount disk with new quota rules
 sudo mount -o remount /
 # Run quotacheck to create aquota.group and aquota.user files
